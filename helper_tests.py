7298efbcadc373aad74590c945fcb9fc817389ed
Christian Bäckfeldt <backfeldt@live.se>
2023-03-02 15:02:39 +0100

    newtestwoo
    inga ideer :(

--- helper_tests.py
+++ helper_tests.py
@@ -1,4 +1,7 @@
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
 
 def simple_assert(gotten_val, expected_val):
     assert gotten_val == expected_val, f"Assertion failed, expected: {expected_val}, got: {gotten_val}"
@@ -6,3 +9,15 @@ def simple_assert(gotten_val, expected_val):
 
 def boolean_assert(value, message):
     assert value, message
+def remove_non_numbers(str1):
+    try:
+        return int(''.join(i for i in str1 if i.isdigit()))
+    except:
+        print("Error value in function!")
+        return 0
+def click_by_xpath(driver,xpath):
+    WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.XPATH, xpath))
+    driver.find_element(By.XPATH,xpath).click()
+def click_by_css(driver,css):
+    WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CSS_SELECTOR, css))
+    driver.find_element(By.CSS_SELECTOR,css).click()
--- inet_test.py
+++ inet_test.py
@@ -3,9 +3,12 @@ from selenium.webdriver.common.by import By
 import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 
-import helper_tests
+import helper_tests as ht
 
 INET_SITE = "https://www.inet.se/"
+
+#datorer_xpath_side = '//*[@id="react-root"]/div[3]/div/div/div[2]/div[2]/div[3]/div/a[1]/div'
+datorer_xpath_side = '//span[normalize-space()="Datorer"]'
 
 class TestClass:
     @pytest.fixture(scope="class")
@@ -23,11 +26,17 @@ class TestClass:
     def get_inet_site(self, load_driver):
 
         driver = load_driver
+        driver.delete_all_cookies()
 
         driver.get(INET_SITE)
 
-        cookies_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div/button[1]")
-        cookies_button.click()
+        #cookies_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/div/button[1]")
+        #cookies_button.click()
+        try:
+            ht.click_by_xpath(driver,'//button[normalize-space()="Jag förstår"]')
+        except:
+            pass
+
 
         yield driver
 
@@ -35,23 +44,36 @@ class TestClass:
 
         driver.delete_all_cookies()
         
-    def test_1(self, get_inet_site):
+    def test_1_assert_url_homepage(self, get_inet_site):
         driver = get_inet_site
 
-        helper_tests.boolean_assert("inet" in driver.current_url, f"Expected inet in url, got: {driver.current_url}")
+        ht.boolean_assert("https://www.inet.se/" in driver.current_url, f"Expected inet in url, got: {driver.current_url}")
+        ht.boolean_assert("För tekniken vi älskar - Inet.se" in driver.title,f"Expected Inet.se in title got:{driver.title}")
     
-    def test_advanced_2(self, get_inet_site):
+    def test_2_datorer_tab(self, get_inet_site):
         driver = get_inet_site
 
-        datorer_link = driver.find_element(By.XPATH, '//*[@id="react-root"]/div[3]/div/div/div[2]/div[2]/div[3]/div/a[1]/div')
-
-        datorer_link.click()
+        ht.click_by_xpath(driver,datorer_xpath_side)
 
         h1 = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "h1"))
         expected_h1 = "Datorer"
 
-        helper_tests.boolean_assert(expected_h1 in h1.text, f"Expected {expected_h1} in text for h1, got: {h1.text}")
+        ht.boolean_assert(expected_h1 in h1.text, f"Expected {expected_h1} in text for h1, got: {h1.text}")
 
         expected_title = "Datorer - Köp online här"
 
-        helper_tests.boolean_assert(expected_title in driver.title, f"Expected {expected_title} in title, got: {driver.title}")
+        ht.boolean_assert(expected_title in driver.title, f"Expected {expected_title} in title, got: {driver.title}")
+
+    def test_3_datorer_under_tabs(self,get_inet_site):
+        driver = get_inet_site
+
+        ht.click_by_css(driver,"[aria-label='expandera kategorin Datorer']")
+        ht.click_by_css(driver,"[aria-label='expandera kategorin Bärbar dator']")
+        ht.click_by_css(driver,"[aria-label='expandera kategorin Chromebook']")
+        ht.click_by_css(driver,"[aria-label='expandera kategorin Chromebook']")
+        ht.click_by_xpath(driver,"/html/body/div[1]/div[3]/div/div/div[1]/div/div[2]/ol/li[3]/div[2]/div/ol/li[1]/div[2]/div/ol/li[1]/div[2]/div/ol/li/a")
+        h1 = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "h1"))
+        expected_h1 = "Chromebook - ASUS"
+        ht.boolean_assert(expected_h1 in h1.text, f"Expected {expected_h1} in text for h1, got: {h1.text}")
+
+        
