5f246138b1a62f6ef00bc5e8c13095f68a21823d
Christian Bäckfeldt <backfeldt@live.se>
2023-03-06 14:04:55 +0100

    little commit(been sick)
    Magsjuka är inte kul

--- inet_test.py
+++ inet_test.py
@@ -9,6 +9,8 @@ INET_SITE = "https://www.inet.se/"
 
 #datorer_xpath_side = '//*[@id="react-root"]/div[3]/div/div/div[2]/div[2]/div[3]/div/a[1]/div'
 datorer_xpath_side = '//span[normalize-space()="Datorer"]'
+number_in_cart = "#cart-button > div > svg > text"
+
 
 class TestClass:
     @pytest.fixture(scope="class")
@@ -29,6 +31,7 @@ class TestClass:
         driver.delete_all_cookies()
 
         driver.get(INET_SITE)
+        driver.maximize_window()
 
         #cookies_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/div/button[1]")
         #cookies_button.click()
@@ -57,11 +60,9 @@ class TestClass:
 
         h1 = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "h1"))
         expected_h1 = "Datorer"
-
         ht.boolean_assert(expected_h1 in h1.text, f"Expected {expected_h1} in text for h1, got: {h1.text}")
 
         expected_title = "Datorer - Köp online här"
-
         ht.boolean_assert(expected_title in driver.title, f"Expected {expected_title} in title, got: {driver.title}")
 
     def test_3_datorer_under_tabs(self,get_inet_site):
@@ -75,5 +76,15 @@ class TestClass:
         h1 = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "h1"))
         expected_h1 = "Chromebook - ASUS"
         ht.boolean_assert(expected_h1 in h1.text, f"Expected {expected_h1} in text for h1, got: {h1.text}")
+        ht.boolean_assert("chromebook" in driver.current_url,f"Expected chromebook in url, got :{driver.current_url}")
+
+    def test_4_add_to_cart(self,get_inet_site):
+        driver = get_inet_site
+
+        ht.click_by_xpath(driver,datorer_xpath_side)
+        #Breaks here for some reason sometimes?
+        ht.click_by_xpath(driver,'//button[normalize-space()="Köp"]')
+        current_items_in_cart = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.CSS_SELECTOR, number_in_cart).text)
+        ht.boolean_assert(current_items_in_cart == "1",f"Expected 1 item in cart, got:{current_items_in_cart}")
 
         
