3b4b9180516a2089cb52d8a1d155ef8fc5e681ae
Christian Bäckfeldt <backfeldt@live.se>
2023-03-23 14:24:23 +0100

    testcommit

--- inet_test.py
+++ inet_test.py
@@ -153,7 +153,7 @@ class TestClass:
         ht.click_by_xpath(driver,'//button[normalize-space()="Töm"]')
         result_text = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/nav/div/div/div[6]/div/div/div/p").text
         ht.boolean_assert(result_text == "Din kundvagn är tom",f"Expected Din kundvagn är tom, got {result_text}")
-        
+
     def test_10_search_specific_product(self,get_inet_site):
         driver = get_inet_site
         product = "AMD Radeon RX 6950 XT MBA"
